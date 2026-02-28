44be108408e76995ab6f75ca725fb13442cb23d8
Aaleya Boxwala <meademigod1808@gmail.com>
2026-02-28 16:59:07 +0530

    fixed bug in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,6 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 
-import CodeEditor from "@/components/CodeEditor";
 
 
 
